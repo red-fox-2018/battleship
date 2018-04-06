9be0263ae287dc1d1ef5bc61776cefb664fae80e
alprak <you@example.com>
2018-04-06 20:21:02 +0700

    baru generate board

--- battleship.js
+++ battleship.js
@@ -1,1 +1,24 @@
 // Your code here
+
+function generateBoard() {
+var multiDim=[];
+var store='';	
+	for(var i=0;i<10;i++) {
+			var arr=[];
+		for(var j=0;j<10;j++) {
+			//store+="s"
+			arr.push(' ');
+		}	
+	multiDim.push(arr);
+	store='';	
+	}
+
+	//push aircrat
+	
+//aircraft
+	
+	
+return multiDim	
+}
+
+console.log(generateBoard());
